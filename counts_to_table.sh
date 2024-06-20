8f7ea1a9c2e36b9a2aad8fa235fd8693ea2c6572
jengelhorn <60544247+jengelhorn@users.noreply.github.com>
2024-06-20 10:15:25 +0200

    Update counts_to_table.sh
    annotation improved, labelling of no peak regions uniform now, accepts peak files from other outputs than MACS now

--- counts_to_table.sh
+++ counts_to_table.sh
@@ -29,11 +29,11 @@ echo "sort and name peaks"
 
 #Peaks are taken from Narrowpeak files of MACS3
 
-for tr in ${C1};  do sort -k1,1 -k2,2n ${PeakC1} | gawk -v OFS='\t' '{if($2<$3 && $1!~"scaf-alt"){print $1,$2,$3,$4"_"$5}}' > ${g}.${tr}.peaks.names.sorted.bed ; done
+for tr in ${C1};  do sort -k1,1 -k2,2n ${PeakC1} | gawk -v OFS='\t' '{if($2<$3 && $1!~"scaf-alt"){if($4~"peak"){print $1,$2,$3,$4"_"$5}else{print $1,$2,$3,"peak_"NR}}' > ${g}.${tr}.peaks.names.sorted.bed ; done
 
-for tr in ${C2};  do sort -k1,1 -k2,2n ${PeakC2} | gawk -v OFS='\t' '{if($2<$3 && $1!~"scaf-alt"){print $1,$2,$3,$4"_"$5}}' > ${g}.${tr}.peaks.names.sorted.bed ; done
+for tr in ${C2};  do sort -k1,1 -k2,2n ${PeakC2} | gawk -v OFS='\t' '{if($2<$3 && $1!~"scaf-alt"){if($4~"peak"){print $1,$2,$3,$4"_"$5}else{print $1,$2,$3,"peak_"NR}}' > ${g}.${tr}.peaks.names.sorted.bed ; done
 
-echo "sort count files"
+echo "sort count files, mark heterozygous sites (scaf-alt)"
 
 for tr in ${C1} ${C2}; do gawk -v OFS='\t' -v g=$g '{if($4!~"scaf-alt"){print $0} else{gsub("scaf-alt","scaf",$4);print $1,$2,$3,$4,"het"}}' ${dir}/B73.${g}.${tr}.counts.bed | sort -k1,1 -k2,2n  > B73.${g}.${tr}.counts.sorted.bed; done
 
@@ -42,7 +42,7 @@ for tr in ${C1} ${C2};  do gawk -v OFS='\t' -v g=$g '{if($1!~"scaf-alt"){print $
 
 
 #What we want: Chr(B73)  STop=POS(B73)   ID(NAMchr_NAMPOS)   REFal(B73)   ALTal(NAM)   Count(Either B73 or NAM)    and we want this B73 coordinate sorted
-echo "intersect peaks and sort"
+echo "intersect count files with peaks and sort"
 
 for tr in ${C1} ${C2}; do intersectBed -a B73.${g}.${tr}.counts.sorted.bed -b ${g}.${tr}.peaks.names.sorted.bed -wa -wb -sorted -loj| gawk -v OFS='\t' '{split($4,var,"\."); print $1,$3,var[1]":"var[2],var[4], var[3], $5, $9}' >  B73.${g}.${tr}.ID.count.sort.csv; done
 
@@ -52,9 +52,9 @@ for tr in ${C1} ${C2}; do intersectBed -a ${g}.B73.${tr}.counts.sorted.bed -b ${
 
 #Now make new file with both ref and alt, one WW, one DS:
 
-# Chr(B73)  STop=POS(B73)   ID(NAMchr_NAMPOS)   REFal(B73)   ALTal(NAM)   Count(B73)   Count(NAM) Peak(B73) Peak(NAM)  PF(PF, if both 0 print n.r.)
+# Chr(B73)  STop=POS(B73)   ID(NAMchr_NAMPOS)   REFal(B73)   ALTal(Paternal)   Count(B73)   Count(Pat) Peak(B73) Peak(Pat)  BF(BF (B73/(B73+Pat)), if both 0 print n.r.)
 
-echo "preparing postfrequency files"
+echo "preparing binding frequency files"
 
 for tr in ${C1} ${C2}; do paste B73.${g}.${tr}.ID.count.sort.csv ${g}.B73.${tr}.ID.count.sort.csv | gawk -v OFS='\t' '{if($2==$9){if($6+$13==0){print "B73-"substr($1,5,length($1)-4),$2,$10,$4,$5,$6,$13,$7,$14,"n.r."} else {print "B73-"substr($1,5,length($1)-4),$2,$10,$4,$5,$6,$13,$7,$14,$6/($6+$13)}}}' > B73.${g}.${tr}.PF.csv; done
 
@@ -62,11 +62,11 @@ echo "adding genotypes and depth "
 
 #Add column with genotype, i.e. if that SNP is present in the List
 
-#Chr(B73)  STop=POS(B73) REFal(B73)   ALTal(NAM) ID(NAMchr_NAMPOS) Genotype (0/0 if B73, 1/1 if NAM) Count(B73)   Count(NAM) PF (n.r. if both 0)
+#Chr(B73)  STop=POS(B73) REFal(B73)   ALTal(Pat) ID(Patchr_PatPOS) Genotype (0/0 if B73, 1/1 if Pat) Count(B73)   Count(Pat) BF (n.r. if both 0)
 
-#Add depth information, i.e. calculate number of reads B73 and NAM and change PF to n.p. post frequency if none of the alleles has a peak at this position
+#Add depth information, i.e. calculate number of reads B73 and Pat and change BF to "n.p. binding frequency" if none of the alleles has a peak at this position
 
-#Chr(B73)  STop=POS(B73) REFal(B73)  ALTal(NAM) ID(NAMchr_NAMPOS) Genotype (0/0 if B73, 1/1 if NAM) norm_Count(B73)   norm_Count(NAM) PEAK(B73) Peak(NAM)  PF (n.p. if no peak in either B73 or NAM) read_Count(B73) read_Count(NAM)
+#Chr(B73)  STop=POS(B73) REFal(B73)  ALTal(Pat) ID(Patchr_PatPOS) Genotype (0/0 if B73, 1/1 if Pat) norm_Count(B73)   norm_Count(Pat) PEAK(B73) Peak(Pat)  BF (n.p. if no peak in either B73 or Pat) read_Count(B73) read_Count(Pat)
 
 #Determine value per read first
 
@@ -75,5 +75,4 @@ CD=`zcat ${bed2} | awk -v OFS="\t" 'BEGIN{n=10000} {if($4>0 && $4<n){n=$4}} END 
 
 for tr in ${C1} ; do gawk -v OFS='\t' -v g=$g -v CW=${CW} '{if(NR==FNR) {a[$1"ü"$2]=$1"ü"$2; text[$1"ü"$2]=$0; next} if(substr($1,5,length$1-4)"ü"$2-1 in a){if($8~"peak" || $9~"peak"){print $1,$2,$4,$5,$3,"1/1",$6,$7,$8,$9,$10,int(($6/CW)+0.5),int(($7/CW)+0.5)} else {print $1,$2,$4,$5,$3,"1/1",$6,$7,$8,$9,"n.p."$10,int(($6/CW)+0.5),int(($7/CW)+0.5)}} else {if($8~"peak" || $9~"peak"){print $1,$2,$4,$5,$3,"0/0",$6,$7,$8,$9,$10,int(($6/CW)+0.5),int(($7/CW)+0.5)} else{print $1,$2,$4,$5,$3,"0/0",$6,$7,$8,$9,"n.p."$10,int(($6/CW)+0.5),int(($7/CW)+0.5)}}}' ${tsv} B73.${g}.${tr}.PF.csv  > B73.${g}.${tr}.PF.GT.RN.csv; done
 
-for tr in ${C2} ; do gawk -v OFS='\t' -v g=$g -v CD=${CD} '{if(NR==FNR) {a[$1"ü"$2]=$1"ü"$2; text[$1"ü"$2]=$0; next} if(substr($1,5,length$1-4)"ü"$2-1 in a){if($8~"peak" || $9~"peak"){print $1,$2,$4,$5,$3,"1/1",$6,$7,$8,$9,$10,int(($6/CD)+0.5),int(($7/CD)+0.5)} else {print $1,$2,$4,$5,$3,"1/1",$6,$7,$8,$9,"n.p."$10,int(($6/CD)+0.5),int(($7/CD)+0.5)}} else {if($8~"peak" || $9~"peak"){print $1,$2,$4,$5,$3,"0/0",$6,$7,$8,$9,$10,int(($6/CD)+0.5),int(($7/CD)+0.5)} else{print $1,$2,$4,$5,$3,"0/0",$6,$7,$8,$9,"n.d."$10,int(($6/CD)+0.5),int(($7/CD)+0.5)}}}' ${tsv} B73.${g}.${tr}.PF.csv  > B73.${g}.${tr}.PF.GT.RN.csv; done
-
+for tr in ${C2} ; do gawk -v OFS='\t' -v g=$g -v CD=${CD} '{if(NR==FNR) {a[$1"ü"$2]=$1"ü"$2; text[$1"ü"$2]=$0; next} if(substr($1,5,length$1-4)"ü"$2-1 in a){if($8~"peak" || $9~"peak"){print $1,$2,$4,$5,$3,"1/1",$6,$7,$8,$9,$10,int(($6/CD)+0.5),int(($7/CD)+0.5)} else {print $1,$2,$4,$5,$3,"1/1",$6,$7,$8,$9,"n.p."$10,int(($6/CD)+0.5),int(($7/CD)+0.5)}} else {if($8~"peak" || $9~"peak"){print $1,$2,$4,$5,$3,"0/0",$6,$7,$8,$9,$10,int(($6/CD)+0.5),int(($7/CD)+0.5)} else{print $1,$2,$4,$5,$3,"0/0",$6,$7,$8,$9,"n.p."$10,int(($6/CD)+0.5),int(($7/CD)+0.5)}}}' ${tsv} B73.${g}.${tr}.PF.csv  > B73.${g}.${tr}.PF.GT.RN.csv; done
