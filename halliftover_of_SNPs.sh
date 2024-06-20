e296c62bb27d126961eb14a69f1abbeaca553a9a
jengelhorn <60544247+jengelhorn@users.noreply.github.com>
2024-06-20 10:09:52 +0200

    Update halliftover_of_SNPs.sh

--- halliftover_of_SNPs.sh
+++ halliftover_of_SNPs.sh
@@ -14,17 +14,13 @@ dir=$2
 hal=$3
 SNPs=$4
 
-export PATH="/netscratch/dep_psl/grp_frommer/Thomas/bin/bedops/bin:$PATH"
 
  cd ${dir}
 
-##tsv file contains 0-based positions
 
 echo "halLiftover"
  
 halLiftover  --hdf5InMemory ${hal} B73 ${SNPs} ${g} ${g}.B73.hallifted.all.bed
-
-
 
 
 
